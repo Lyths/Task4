55dc5e96cc5275f23a1caca61a7f787f61178a06
Lyths <mukhtar234@mail.ru>
2023-09-20 17:12:22 +0300

    Success
    Successful

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/ok")
+	public String ok()
+	{
+		return "OK!";
+	}
+
+	@PostMapping("/post")
+	public ResponseEntity<Model> post(@RequestBody Model model)
+	{
+		model.Content = model.Content.concat(" Response");
+		return ResponseEntity.ok(model);
+	}
+}
---
+++ demo/src/main/java/com/example/demo/Model.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import java.time.LocalDateTime;
+
+public class Model {
+    public String Content;
+    public LocalDateTime DateTime = LocalDateTime.now();
+}
